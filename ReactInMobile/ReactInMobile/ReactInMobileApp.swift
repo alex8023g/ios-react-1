fc370f440f94720cf50bd7b841005c56e5d444cf
alex8023 <alex8023@yandex.ru>
2023-12-22 11:52:43 +0300

    start project, doesn`t work

---
+++ ReactInMobile/ReactInMobile/ReactInMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactInMobileApp.swift
+//  ReactInMobile
+//
+//  Created by j on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReactInMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReactInMobile/ReactInMobile/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  ReactInMobile
+//
+//  Created by j on 22.12.2023.
+//
+
+import Foundation
